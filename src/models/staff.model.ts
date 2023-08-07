3ccd6005186e03cb9da7480903bf1fcef8916354
muhammadtanko <teekay014@gmail.com>
2023-08-07 23:17:48 +0100

    project setup

---
+++ src/models/staff.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model, Document } from "mongoose";
+export  enum Gender {
+    Male = "male",
+    Female = "female"
+}
+interface Biodata {
+    gender: Gender,
+    dateOfBirth: Date
+}
+export interface IStaff extends Document {
+    id: string,
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    biodata: Biodata,
+    dateOfEngagement: Date,
+
+}
+const StaffSchema: Schema = new Schema<IStaff>({
+    id: { type: String, unique: true },
+    firstName: { type: String},
+    middleName: { type: String },
+    lastName: { type: String },
+    biodata: {
+        gender: { type: String, enum: Object.values(Gender) },
+        dateOfBirth: { type: Date },
+    },
+    dateOfEngagement: { type: Date }
+})
+export const StaffModel = model<IStaff>("Staff", StaffSchema);
